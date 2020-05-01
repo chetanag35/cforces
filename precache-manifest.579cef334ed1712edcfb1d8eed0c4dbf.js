e7553fab0269f34a78bd8004bfb46445d2519de1
chetanag35 <chetanag35@gmail.com>
2020-05-01 10:27:04 +0530

    Updates

---
+++ precache-manifest.579cef334ed1712edcfb1d8eed0c4dbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e58c61769b867846fa97b5d1a810ed6",
+    "url": "/cforces/index.html"
+  },
+  {
+    "revision": "dd99ec468a268cbd0f4a",
+    "url": "/cforces/static/css/main.3f023190.chunk.css"
+  },
+  {
+    "revision": "3209543fd5149493ec8f",
+    "url": "/cforces/static/js/2.ed6ae9c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cforces/static/js/2.ed6ae9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd99ec468a268cbd0f4a",
+    "url": "/cforces/static/js/main.e27f94ea.chunk.js"
+  },
+  {
+    "revision": "618d0183b274432ddfc0",
+    "url": "/cforces/static/js/runtime-main.f7422be2.js"
+  }
+]);
